de6f3b6fc3969b80c20a4d768547bf2f4b8c4f9d
Shruti Choudhary <shruti.choudhary@grazitti.com>
2024-12-10 17:54:24 +0530

    footer section added

---
+++ blocks/footer/footer.js
@@ -0,0 +1,4 @@
+const row1= document.querySelector('.footer.block > div:first-child');
+row1.classList.add('row1');
+const row2= document.querySelector('.footer.block > div:nth-of-type(2)');
+row2.classList.add('row2');
